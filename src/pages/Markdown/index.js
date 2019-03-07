76398285b92e04ec80500ce017362759ed954428
liuyixiang <liuyixiang@xiaomi.com>
2019-03-07 11:13:35 +0800

    添加样式

--- src/pages/Markdown/index.js
+++ src/pages/Markdown/index.js
@@ -34,7 +34,7 @@ export default class Markdown extends Component {
 
   textarea() {
     return (
-      <div className="field">
+      <div className="field left">
         <div className="control">
           <textarea ref={this.myRef} className="textarea is-primary markdown-body" placeholder="Primary textarea"
             value={this.state._data}
@@ -48,7 +48,7 @@ export default class Markdown extends Component {
   markdown() {
     const html = marked(this.state._data);
     return (
-      <div className="field">
+      <div className="field right">
         <div className="control">
           <div className="textarea is-info markdown-body"
             dangerouslySetInnerHTML={{__html: html}}
@@ -58,27 +58,48 @@ export default class Markdown extends Component {
     )  
   }
 
-  blod() {
+  handle(field) {
     var txt = this.myRef.current;
     var start = txt.selectionStart;
     var end = txt.selectionEnd;
     if(start !== end) {
-      var md = this.state._data;
-      md = md.substring(0, start) + '**' + md.substring(start, end) + "**" + md.substring(end);
-      this.setState({_data: md})
+      if(field === 'blob') {
+        let md = this.state._data;
+        md = md.substring(0, start) + '**' + md.substring(start, end) + "**" + md.substring(end);
+        this.setState({_data: md})
+      } else if (field === 'del') {
+        let md = this.state._data;
+        md = md.substring(0, start) + '~~' + md.substring(start, end) + "~~" + md.substring(end);
+        this.setState({_data: md})        
+      } else if (field === 'yinyong') {
+        let md = this.state._data;
+        md = md.substring(0, start) + '\n > ' + md.substring(start, end) + "\n" + md.substring(end);
+        this.setState({_data: md})         
+      }
+ 
     }
-
-
   }
+
+
 
 
   render() {
 
     return (
       <div>
-        <span className="fas fa-border">
-          <i className="iconfont icon-zitijiacu" onClick={this.blod.bind(this)}></i>
-        </span>
+        <div className="notification is-primary">
+          <button className="delete"></button>
+          Markdown 文本编辑器
+        </div>        
+        <a title="加粗" onClick={this.handle.bind(this, 'blob')} className="button is-primary is-outlined is-small">
+          <i className="iconfont icon-zitijiacu"></i>
+        </a>
+        <a title="中划线" onClick={this.handle.bind(this, 'del')} className="button is-primary is-outlined is-small">
+          <i className="iconfont icon-zitishanchuxian"></i>
+        </a>
+        <a title="引用" onClick={this.handle.bind(this, 'yinyong')} className="button is-primary is-outlined is-small">
+          <i className="iconfont icon-yinyong"></i>
+        </a>                        
         {this.textarea()}
         {this.markdown()}
       </div>
