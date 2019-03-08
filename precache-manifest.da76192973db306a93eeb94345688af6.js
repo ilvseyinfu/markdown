284bb279c8bcc663b1a0d41409f20cf931a1a5d3
liuyixiang <liuyixiang@xiaomi.com>
2019-03-08 15:47:51 +0800

    Updates

---
+++ precache-manifest.da76192973db306a93eeb94345688af6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "cecd068acbcae8865ef7",
+    "url": "/static/js/main.cecd068a.chunk.js"
+  },
+  {
+    "revision": "289f141646f5bb07efe9",
+    "url": "/static/js/2.289f1416.chunk.js"
+  },
+  {
+    "revision": "cecd068acbcae8865ef7",
+    "url": "/static/css/main.1d39efe9.chunk.css"
+  },
+  {
+    "revision": "289f141646f5bb07efe9",
+    "url": "/static/css/2.2c544340.chunk.css"
+  },
+  {
+    "revision": "314ff4cfbcb7c186439c885f79414a31",
+    "url": "/index.html"
+  }
+];
