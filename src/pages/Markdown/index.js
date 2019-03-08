1b8b1bc94d046d2e3ade8ea5f1342d01d207f355
liuyixiang <liuyixiang@xiaomi.com>
2019-03-08 10:21:18 +0800

    引入eslint

--- src/pages/Markdown/index.js
+++ src/pages/Markdown/index.js
@@ -5,92 +5,54 @@ import 'bulma/css/bulma.css';
 import './style.css';
 
 export default class Markdown extends Component {
-
-
   constructor(props) {
     super(props);
     this.state = {
-      _data: ''
-    }
+      sourceData: ''
+    };
     this.myRef = React.createRef();
-
   }
+
   change(field, e) {
     this.setState({
       [field]: e.target.value
     }, () => {
-      localStorage.setItem('_data', this.state._data);  
-    })
-
-    
+      localStorage.setItem('sourceData', this.state.sourceData);
+    });
   }
 
   componentDidMount() {
-    var _data = localStorage.getItem('_data');
-    if(_data) {
-      this.setState({_data})
+    const sourceData = localStorage.getItem('sourceData');
+    if (sourceData) {
+      this.setState({ sourceData });
     }
-  }
-
-  textarea() {
-    return (
-      <div className="field left">
-        <div className="control">
-          <textarea ref={this.myRef} className="textarea is-primary markdown-body" placeholder="Primary textarea"
-            value={this.state._data}
-            onChange={this.change.bind(this, '_data')}
-          />
-        </div>
-      </div>
-    )
-  }
-
-  markdown() {
-    const html = marked(this.state._data);
-    return (
-      <div className="field right">
-        <div className="control">
-          <div className="textarea is-info markdown-body"
-            dangerouslySetInnerHTML={{__html: html}}
-          ></div>
-        </div>
-      </div>      
-    )  
   }
 
   handle(field) {
-    var txt = this.myRef.current;
-    var start = txt.selectionStart;
-    var end = txt.selectionEnd;
-    if(start !== end) {
-      if(field === 'blob') {
-        let md = this.state._data;
-        md = md.substring(0, start) + '**' + md.substring(start, end) + "**" + md.substring(end);
-        this.setState({_data: md})
+    const txt = this.myRef.current;
+    const start = txt.selectionStart;
+    const end = txt.selectionEnd;
+    let md = this.state.sourceData;
+    if (start !== end) {
+      if (field === 'blob') {
+        md = md.substring(0, start) + '**' + md.substring(start, end) + '**' + md.substring(end);
       } else if (field === 'del') {
-        let md = this.state._data;
-        md = md.substring(0, start) + '~~' + md.substring(start, end) + "~~" + md.substring(end);
-        this.setState({_data: md})        
+        md = md.substring(0, start) + '~~' + md.substring(start, end) + '~~' + md.substring(end);
       } else if (field === 'yinyong') {
-        let md = this.state._data;
-        md = md.substring(0, start) + '\n > ' + md.substring(start, end) + "\n" + md.substring(end);
-        this.setState({_data: md})         
+        md = md.substring(0, start) + '\n > ' + md.substring(start, end) + '\n' + md.substring(end);
       }
- 
     }
+    this.setState({ sourceData: md });
   }
 
-
-
-
   render() {
-
+    const html = marked(this.state.sourceData);
     return (
       <div>
         <div className="notification is-primary">
           <button className="delete"></button>
           Markdown 文本编辑器
-        </div>        
+        </div>
         <a title="加粗" onClick={this.handle.bind(this, 'blob')} className="button is-primary is-outlined is-small">
           <i className="iconfont icon-zitijiacu"></i>
         </a>
@@ -99,10 +61,23 @@ export default class Markdown extends Component {
         </a>
         <a title="引用" onClick={this.handle.bind(this, 'yinyong')} className="button is-primary is-outlined is-small">
           <i className="iconfont icon-yinyong"></i>
-        </a>                        
-        {this.textarea()}
-        {this.markdown()}
+        </a>
+        <div className="field left">
+          <div className="control">
+            <textarea ref={this.myRef} className="textarea is-primary markdown-body" placeholder="Primary textarea"
+              value={this.state.sourceData}
+              onChange={this.change.bind(this, 'sourceData')}
+            />
+          </div>
+        </div>
+        <div className="field right">
+          <div className="control">
+            <div className="textarea is-info markdown-body"
+              dangerouslySetInnerHTML={{ __html: html }}
+            ></div>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
