bde095e0fc08738d5f9df2fd8b52a0426cbf930f
liuyixiang <liuyixiang@xiaomi.com>
2019-03-08 16:08:49 +0800

    Updates

---
+++ precache-manifest.aec72a15c0da4df139ea82d109b593f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "33525288523c4d40e61f",
+    "url": "/markdown/static/js/runtime~main.33525288.js"
+  },
+  {
+    "revision": "fff49a864ac36b66b5df",
+    "url": "/markdown/static/js/main.fff49a86.chunk.js"
+  },
+  {
+    "revision": "289f141646f5bb07efe9",
+    "url": "/markdown/static/js/2.289f1416.chunk.js"
+  },
+  {
+    "revision": "fff49a864ac36b66b5df",
+    "url": "/markdown/static/css/main.1d39efe9.chunk.css"
+  },
+  {
+    "revision": "289f141646f5bb07efe9",
+    "url": "/markdown/static/css/2.2c544340.chunk.css"
+  },
+  {
+    "revision": "1043a4ef1830e1dfb132fc1fe111f2ee",
+    "url": "/markdown/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.da76192973db306a93eeb94345688af6.js"
+  "/markdown/precache-manifest.aec72a15c0da4df139ea82d109b593f9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/markdown/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
